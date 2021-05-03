2d99cd6bc14178c488e4faebfb67926584fe9fab
hemanthmamidi <mamidihemanth243@gmail.com>
2021-05-03 20:18:05 +0530

    added filter

--- src/main/java/com/mango/spring/jwt_token/config/SecurityConfig.java
+++ src/main/java/com/mango/spring/jwt_token/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        auth.userDetailsService(myUserDetailsService);
     }
 
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
